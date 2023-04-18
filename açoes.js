45dbdc12b5c5bebe98edacaae3c42abbd2342bbf
BRENO LOPES DE NOVAES <128421985+Brenolopesdenovaes@users.noreply.github.com>
2023-04-18 12:45:38 -0300

    .

--- açoes.js
+++ açoes.js
@@ -1,15 +1,4 @@
-const btn = document.getElementById("btn");
-const secaoQuem = document.getElementById("quem");
 
-btn.addEventListener("click", () => {
-  if (secaoQuem.style.width === "30%") {
-    secaoQuem.style.width = "10%";
-    btn.style.left = "110%";
-  } else {
-    secaoQuem.style.width = "30%";
-    btn.style.left = "160%";
-  }
-});
 
 
 
